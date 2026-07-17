e5a6c67f805d084e0c216b43d8ef2011c686e6f3
Tanner Kvarfordt <tanner.kvarfordt@proton.me>
2026-07-17 16:41:04 -0700

    Updated lspconfig

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -122,19 +122,27 @@ return {
       --  See `:help lsp-config` for information about keys and how to configure
       ---@type table<string, vim.lsp.Config>
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        -- Tools (linters, analyzers, formatters, etc.)
+        stylua = {},
+        clang_format = {},
+        autopep8 = {},
+        shellharden = {},
+        xmlformatter = {},
+        yamlfmt = {},
+        ansible_lint = {},
+        cmakelint = {},
+        gitlint = {},
+        hadolint = {},
+        jsonlint = {},
+        markdownlint = {},
+        pylint = {},
+        shellcheck = {},
+        systemdlint = {},
+        delve = {},
+        vale = {},
 
-        stylua = {}, -- Used to format Lua code
-
+        -- Language servers
+        ansiblels = {},
         -- Special Lua Config, as recommended by neovim help docs
         lua_ls = {
           on_init = function(client)
@@ -168,6 +176,76 @@ return {
             },
           },
         },
+        bashls = {},
+        clangd = {},
+        cmake = {},
+        docker_compose_language_service = {},
+        dockerls = {},
+        eslint = {},
+        gopls = {},
+        html = {},
+        jinja_lsp = {},
+        jqls = {},
+        jsonls = {},
+        markdown_oxide = {},
+        powershell_es = {},
+        pyright = {},
+        rust_analyzer = {},
+        sqls = {},
+        vimls = {},
+        yamlls = {
+          settings = {
+            yaml = {
+              -- Schema configuration
+              schemas = {
+                ['https://json.schemastore.org/github-workflow.json'] = {
+                  '.github/workflows/*.yml',
+                  '.github/workflows/*.yaml',
+                },
+                ['https://json.schemastore.org/docker-compose.json'] = {
+                  'docker-compose*.yml',
+                  'docker-compose*.yaml',
+                  'compose*.yml',
+                  'compose*.yaml',
+                },
+                ['https://json.schemastore.org/kubernetes-1.24.json'] = {
+                  '*.k8s.yaml',
+                  '*.k8s.yml',
+                  '**/k8s/**/*.yaml',
+                  '**/k8s/**/*.yml',
+                },
+                ['https://json.schemastore.org/ansible-playbook.json'] = {
+                  '*playbook*.yml',
+                  '*playbook*.yaml',
+                  '**/ansible/**/*.yml',
+                  '**/ansible/**/*.yaml',
+                },
+                ['https://json.schemastore.org/gitlab-ci.json'] = {
+                  '.gitlab-ci.yml',
+                  '**/.gitlab-ci.yml',
+                },
+              },
+
+              -- Enable schema store for additional schemas
+              schemaStore = {
+                enable = true,
+              },
+
+              -- Validation settings (keep true for syntax/schema errors)
+              validate = true,
+              completion = true,
+              hover = true,
+
+              -- Formatting (disable if you want to control formatting separately)
+              format = {
+                enable = false,
+                singleQuote = true,
+              },
+            },
+          },
+        },
+
+
       }
 
       -- Ensure the servers and tools above are installed
