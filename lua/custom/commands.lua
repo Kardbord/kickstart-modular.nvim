8c743b504e373817c67ac47a1b7b2f5a078b7088
Tanner Kvarfordt <tanner.kvarfordt@proton.me>
2026-07-17 16:15:15 -0700

    nvim :Update function now does not show a UI

--- lua/custom/commands.lua
+++ lua/custom/commands.lua
@@ -1,6 +1,20 @@
-vim.api.nvim_create_user_command('UpdateAll', function()
-  vim.treesitter.language.update()
-  require('mason-registry').update()
-  require('lazy').update()
-end, { desc = 'Update all plugins and dependencies (Treesitter, Mason, Lazy)' })
+vim.api.nvim_create_user_command('Update', function()
+  local function log(msg)
+    vim.notify(msg)
+    vim.cmd('redraw')
+  end
 
+  log('Updating Treesitter parsers...')
+  require('nvim-treesitter.install').update { with_sync = true }
+
+  log('Updating Mason packages...')
+  require('mason-registry').update()
+
+  log('Cleaning unused plugins...')
+  require('lazy').clean { show = false }
+
+  log('Updating Lazy plugins...')
+  require('lazy').update { show = false }
+
+  log('Update complete.')
+end, { desc = 'Update all plugins and dependencies (Treesitter, Mason, Lazy)' })
