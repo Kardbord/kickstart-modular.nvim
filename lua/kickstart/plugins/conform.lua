79ed36ae78a2a276d95f711f54a8fbbf7705b479
Tanner Kvarfordt <tanner.kvarfordt@proton.me>
2026-08-06 10:10:23 -0700

    Auto format on save

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -18,16 +18,9 @@ return {
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
-        -- You can specify filetypes to autoformat on save here:
-        local enabled_filetypes = {
-          -- lua = true,
-          -- python = true,
-        }
-        if enabled_filetypes[vim.bo[bufnr].filetype] then
-          return { timeout_ms = 500 }
-        else
-          return nil
-        end
+        -- conform only formats when an external formatter or LSP is available;
+        -- otherwise this is a no-op, so files without a tool are left untouched.
+        return { timeout_ms = 500 }
       end,
       default_format_opts = {
         lsp_format = 'fallback', -- Use external formatters if configured below, otherwise use LSP formatting. Set to `false` to disable LSP formatting entirely.
