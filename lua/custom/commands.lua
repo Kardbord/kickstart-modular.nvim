235ce352d5ca410479c5e7b47eec1b0dc3de97d5
Tanner Kvarfordt <tanner.kvarfordt@proton.me>
2026-09-03 10:40:19 -0700

    Add Lsp* aliases back for convenience

--- lua/custom/commands.lua
+++ lua/custom/commands.lua
@@ -1,3 +1,32 @@
+vim.api.nvim_create_user_command('LspEnable', function(opts)
+  vim.cmd('lsp enable' .. (opts.args and ' ' .. opts.args or ''))
+end, { desc = 'Enable LSP config for current buffer (alias for :lsp enable)', bang = true, nargs = '?' })
+
+vim.api.nvim_create_user_command('LspDisable', function(opts)
+  vim.cmd('lsp disable' .. (opts.args and ' ' .. opts.args or ''))
+end, { desc = 'Disable LSP config for current buffer (alias for :lsp disable)', bang = true, nargs = '?' })
+
+vim.api.nvim_create_user_command('LspStop', function(opts)
+  vim.cmd('lsp stop' .. (opts.args and ' ' .. opts.args or ''))
+end, { desc = 'Stop LSP client(s) for current buffer (alias for :lsp stop)', bang = true, nargs = '?' })
+
+vim.api.nvim_create_user_command('LspRestart', function(opts)
+  vim.cmd('lsp restart' .. (opts.args and ' ' .. opts.args or ''))
+end, { desc = 'Restart LSP client(s) for current buffer (alias for :lsp restart)', bang = true, nargs = '?' })
+
+vim.api.nvim_create_user_command('LspInfo', function()
+  vim.cmd('checkhealth vim.lsp')
+end, { desc = 'Show LSP client and server status (alias for :checkhealth vim.lsp)' })
+
+vim.api.nvim_create_user_command('LspLog', function()
+  local log_path = vim.lsp.log.get_filename()
+  if log_path then
+    vim.cmd('edit ' .. vim.fn.shellescape(log_path))
+  else
+    vim.notify('No LSP log file found')
+  end
+end, { desc = 'Open the LSP log file' })
+
 vim.api.nvim_create_user_command('Update', function()
   local function log(msg)
     vim.notify(msg)
